59225ba236757ee242238c903ffa2f68bf247ee1
Ethan Martin <ethan@ethanmartin.net>
2018-01-29 19:34:54 -0500

    add cursor movement

--- ned.c
+++ ned.c
@@ -6,7 +6,34 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
+#define VERSION v0.0.1
+
 #define KEY_CTRL(c) ((c)-96)
+
+struct editor_state {
+    int y, x;
+    int maxy, maxx;
+};
+struct editor_state es;
+
+void editor_move(int dir) {
+    switch (dir) {
+        case KEY_UP:
+            es.y--;
+            break;
+        case KEY_DOWN:
+            es.y++;
+            break;
+        case KEY_RIGHT:
+            es.x++;
+            break;
+        case KEY_LEFT:
+            es.x--;
+            break;
+        default:
+            ;
+    }
+}
 
 void screen_shutdown() {
     endwin();
@@ -14,7 +41,7 @@ void screen_shutdown() {
 
 void screen_draw_lines() {
     for (int i = 0; i < getmaxy(stdscr); i++) {
-        mvaddch(i, 0, '~');
+        mvprintw(i, 0, "~");
     }
 }
 
@@ -26,14 +53,22 @@ void screen_update() {
     // draw text
     screen_draw_lines();
 
-    // move cursor back to top
-    move(0, 0);
+    // move cursor back to current location
+    move(es.y, es.x);
+    refresh();
 }
 
 void screen_input() {
     int c = getch();
 
     switch (c) {
+        case KEY_UP:
+        case KEY_DOWN:
+        case KEY_RIGHT:
+        case KEY_LEFT:
+            editor_move('c');
+            break;
+
         case KEY_CTRL('q'):
             exit(0);
 
@@ -51,6 +86,10 @@ int main() {
     keypad(stdscr, TRUE);
     define_key("\b", 8);  // there's some weirdness with Ctrl-H sending some funky backspace char instead of 8, band-aid while investigating this
     atexit(screen_shutdown);
+
+    // set initial editor state
+    es.y = es.x = 0;
+    getmaxyx(stdscr, es.maxy, es.maxx);
 
     while (TRUE) {
         screen_update();
