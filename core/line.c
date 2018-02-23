ecb0af851331e48356b71fb22cfc67c6913ac1ad
Ethan Martin <ethan@ethanmartin.net>
2018-02-23 15:04:56 -0500

    implement line.c and header

---
+++ core/line.c
@@ -0,0 +1,61 @@
+/*
+ * line.c
+ * Contains functions for manipulating lines
+ * Copyright (c) 2018 Ethan Martin
+ */
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "line.h"
+
+/* creates a new, empty line */
+*line empty_line() {
+    *line l;
+
+    l->s = malloc(1);
+    l->s[0] = '\0';
+
+    l->len = 0;
+}
+
+/* cleans up the line */
+void del_line(line *l) {
+    free(line->s);
+    free(line);
+}
+
+/* grow or shrink a line */
+void lresize(line *l, int len) {
+    l->s = realloc(l->s, len + 1);
+    l->s[len] = '\0';
+
+    l->len = len;
+}
+
+/* add a character to the line at the given index */
+void laddch(line *l, const char c, int i) {
+    lresize(l, l->len + 1);
+
+    // offset memory if needed
+    if (i < l->len) {
+        memmove(&l->s[i + 1], &l->s[i], l->len - i);
+    }
+
+    // insert the char
+    l->s[i] = c;
+}
+
+/* add a string to the line at the given index */
+void laddstr(line *l, const char *s, int len, int i) {
+    lresize(l, l->len + len);
+
+    // offset if needed
+    if (i < l->len) {
+        memmove(&l->s[i + len], &l->s[i], l->len - i);
+    }
+
+    // insert the string
+    memcpy(&l->s[i], s, len);
+}
+
---
+++ core/line.h
@@ -0,0 +1,26 @@
+/*
+ * line.h
+ * Contains line struct and declares functions for line manipulation
+ * Copyright (c) 2018 Ethan Martin
+ */
+
+typedef struct line {
+    int len;
+    char *s;
+} line;
+
+/* create a new empty line */
+*line empty_line();
+
+/* free a line */
+void del_line(line *l);
+
+/* grow or shrink a line */
+void lresize(line *l, int len);
+
+/* add a character to the line */
+void laddch(line *l, const char c, int i);
+
+/* add a string to the line */
+void laddstr(line *l, const char *s, int len, int i);
+
