a68741874b021288633f5c074dc1bb0a13a3cbfb
Ethan Martin <ethan@ethanmartin.net>
2018-04-01 19:39:53 -0400

    add attribute struct

--- core/include/core/attribute.h
+++ core/include/core/attribute.h
@@ -4,22 +4,34 @@
  * Copyright (c) 2018 Ethan Martin
  */
 
+#ifndef ATTRIBUTE_H
+#define ATTRIBUTE_H
+
 #include <stdbool.h>
 
 /* define possible attributes */
 enum attr_type {
+    NORMAL,
     BOLD,
     HIGHLIGHT,
     COLOR1,
     COLOR2,
     COLOR3,
     COLOR4
-}
+};
 
 /* contains attribute type, whether it is turned on/off, and location in line */
 typedef struct attribute {
-    enum attr_type;
+    enum attr_type type;
     bool enabled;
     int i;
-}
+} attribute;
+
+/* create new attribute pointer */
+attribute *newattr();
+
+/* free the attribute */
+void delattr(attribute *a);
+
+#endif
 
---
+++ core/src/attribute.c
@@ -0,0 +1,25 @@
+/*
+ * attribute.c
+ * Implements attribute functions
+ * Copyright (c) 2018 Ethan Martin
+ */
+
+#include <stdlib.h>
+
+#include <core/attribute.h>
+
+/* create new attribute with default values */
+attribute *newattr() {
+    attribute *a = malloc(sizeof(attribute));
+    a->type = NORMAL;
+    a->enabled = true;
+    a->i = 0;
+
+    return a;
+}
+
+/* free the attribute */
+void delattr(attribute *a) {
+    free(a);
+}
+
